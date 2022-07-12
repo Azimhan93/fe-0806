cc553c8ff39baa112944999999fad7cf8a0a4831
Orkhan Azimov <leomessigoldenboy@gmail.com>
2022-07-12 18:40:00 +0300

    add calendar_demo

---
+++ Lesson13/script.js
@@ -0,0 +1,75 @@
+function renderCalendar(month, year, calendarElement) {
+
+    const monthName = renderCalendar.MONTHES[month];
+
+    const title = `${monthName} ${year}`;
+
+    calendarElement.querySelector('.calendar_data-title').innerText = title;
+
+    const firstDayOfMonth = new Date();
+
+    firstDayOfMonth.setDate(1);
+    firstDayOfMonth.setMonth(month);
+    firstDayOfMonth.setFullYear(year);
+
+    const firstWeekDayOfMonth = firstDayOfMonth.getDay();
+    const firstRenderDay = new Date(firstDayOfMonth);
+    const firstDayShift = firstWeekDayOfMonth === 0 ? 6 : firstWeekDayOfMonth - 1;
+
+    firstRenderDay.setDate(firstRenderDay.getDate() - firstDayShift);
+
+    const lastDayOfMonth = new Date(firstDayOfMonth);
+
+    lastDayOfMonth.setMonth(month + 1);
+    lastDayOfMonth.setDate(0);
+
+    const lastWeekDayOfMonth = lastDayOfMonth.getDay();
+    const lastRenderDay = new Date(lastDayOfMonth);
+    const lastDayShift = (7 - lastWeekDayOfMonth) % 7;
+
+    lastRenderDay.setDate(lastRenderDay.getDate() + lastDayShift);
+
+    const days = [];
+
+    for (
+        const renderDay = new Date(firstRenderDay);
+        renderDay <= lastRenderDay;
+        renderDay.setDate(renderDay.getDate() + 1)
+    ) {
+        const dayEl = document.createElement('li');
+
+        dayEl.className = 'calendar__day';
+
+        if (renderDay.getMonth() !== month) {
+            dayEl.classList.add('calendar__day--not-in-month');
+        }
+        
+        days.push(dayEl);
+    }
+    const daysContainer = calendarElement.querySelector('.calendar_data-week-ul');
+
+    daysContainer.innerText = '';
+
+    daysContainer.append( ...days );
+}
+
+
+renderCalendar.MONTHES = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const calendarElement = document.querySelector('.calendar');
+let currentDay = new Date();
+
+renderCalendar( currentDay.getMonth(), currentDay.getFullYear(), calendarElement );
